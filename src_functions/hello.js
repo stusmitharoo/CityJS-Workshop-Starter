6c38f8b3ebe41d5106320633b958c770add67b53
stusmitharoo <stuart@streeva.com>
2019-05-01 10:56:33 +0100

    commit

--- src_functions/hello.js
+++ src_functions/hello.js
@@ -1,7 +1,25 @@
+import trainedNet from './trained-net';
+
 exports.handler = function (event, context, callback) {
-  console.log('hello world')
+  let result = []
+  let board = JSON.parse(event.body)
+  const boardSum = board.reduce((a, b) => a + b, 0)
+
+  result = trainedNet(board)
+  console.log(result)
+
+  const emptySpaces = board.map((space, index) => {
+    return space === 0 ? result[index] : null
+  }).filter(space => {
+    return space !== null
+  })
+
+  const response = {
+    index: result.indexOf(emptySpaces.sort(function (a, b) { return b - a })[0])
+  }
+
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
-  });
+    body: JSON.stringify(response)
+  })
 }
